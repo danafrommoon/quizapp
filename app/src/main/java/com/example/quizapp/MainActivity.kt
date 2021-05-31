7e4f035ad2daa6af65eada6f2a1371b2722ea42a
aidana <a.orunbassarova@astanait.edu.kz>
2021-05-31 20:04:12 +0500

    update v.2

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.quizapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -120,6 +120,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                         intent.putExtra(Constants.CORRECT_ANSWERS,mCorrectAnswers)
                         intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionList!!.size)
                         startActivity(intent)
+                        finish()
                         }
                     }
                 }else {
